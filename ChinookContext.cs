7e48f6b3dbe2f5c231396f9d71dffbc8da30b7d8
Juan Matute <juan.matute@gmail.com>
2023-01-26 08:22:14 -0500

    db

--- ChinookContext.cs
+++ ChinookContext.cs
@@ -37,7 +37,7 @@ public partial class ChinookContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=/Users/juanmatute/Projects/Chinook.db;");
+        => optionsBuilder.UseSqlite("Data Source=/Users/juanmatute/Projects/shadow/Chinook.db;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
